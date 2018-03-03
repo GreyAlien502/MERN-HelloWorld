d3ca52557fabd6847d461053af6812e51c953b1c
abluestein <abluestein@RTI-PC05.campus.stonybrook.edu>
2018-03-03 04:36:17 -0500

    Added Main Page and Formatted CSS

--- app/src/components/EventList.js
+++ app/src/components/EventList.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+var myProps;
+
+function getRow(event, index)
+{
+	return <tr key = {index}><th>{event.name}</th><th>{event.location}</th><th>{event.time}</th></tr>;
+}
+
 export const EventList = (props) => (
-            <h1>Henro, <button>{props.name}!</button></h1>
+			<div><h1>U WAN SUM FUD?</h1><a href = '/add'><button id = "addbtn"><h3>add a new event</h3></button></a><table><tbody><tr><th>NAME</th><th>LOCATION</th><th>TIME</th></tr>{props.events.map((x,y) => getRow(x,y))}</tbody></table></div>
         )//return
